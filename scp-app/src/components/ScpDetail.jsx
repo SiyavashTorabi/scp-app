861a7be752721d1dfb4a63814bbae6a101c4e284
siyavashtorabi <vashsiya81@gmail.com>
2021-10-15 03:37:00 -0500

    took off the lock function for easy acces

--- scp-app/src/components/ScpDetail.jsx
+++ scp-app/src/components/ScpDetail.jsx
@@ -2,13 +2,13 @@ import { useState, useEffect } from "react";
 import { useParams, useHistory } from "react-router-dom";
 import { getOneScp } from "../services/api";
 import { deleteScp } from "../services/api";
-import Lock from "./Lock";
+
 
 export default function ScpDetail() {
   const [scp, setScp] = useState({});
   const history = useHistory();
   const { id } = useParams();
-  const [locked, setLocked] = useState(true);
+  
 
   // console.log(id)
   useEffect(() => {
@@ -25,24 +25,21 @@ export default function ScpDetail() {
   };
 
   return (
-    <>
-      {locked ? (
-        <Lock scp={scp} setLocked={setLocked} />
-      ) : (
-        <div className={`scp-info ${id}`}>
-          <p>{scp.fields?.SCP}</p>
-          <p>{scp.fields?.SCP_title}</p>
-          <p>{scp.fields?.object_Class}</p>
-          <p>{scp.fields?.SCP_description}</p>
-          <img alt={scp.fields?.SCP_title} src={scp.fields?.SCP_img} />
-          <a className="Mlink" href={scp.fields?.more_info}>
-            = more info =
-          </a>
-          <button className="dlt" onClick={handleDelet}>
-            delete
-          </button>
-        </div>
-      )}
-    </>
-  );
-}
+    
+    <div className={`scp-info ${id}`}>
+      <p>{scp.fields?.SCP}</p>
+      <p>{scp.fields?.SCP_title}</p>
+      <p>{scp.fields?.object_Class}</p>
+      <p>{scp.fields?.SCP_description}</p>
+      <img alt={scp.fields?.SCP_title} src={scp.fields?.SCP_img} />
+      <a className="Mlink" href={scp.fields?.more_info}>
+        = more info =
+      </a>
+      <button className="dlt" onClick={handleDelet}>
+        delete
+      </button>
+    </div>
+    
+    
+  )
+};
